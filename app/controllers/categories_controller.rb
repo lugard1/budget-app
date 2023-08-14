171278eabfae74c878b582bafc9c8a1d97f3e963
Lugard Agu <agulugard@gmail.com>
2023-08-14 21:53:25 +0100

    Update categories_controller and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -65,6 +65,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.fetch(:category, {})
+      params.require(:category).permit(:name, :icon)
     end
 end
