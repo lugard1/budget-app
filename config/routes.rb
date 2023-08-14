df2204a124e59d14998293cefae9ab513e45a586
Lugard Agu <agulugard@gmail.com>
2023-08-14 21:20:07 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories
+  resources :categories, only: [:index, :show, :new, :create, :destroy]
   devise_for :users
   resources :users
   root 'splash_screens#index'
