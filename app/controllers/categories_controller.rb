ae5d1861c387990b6a8102971e0b4703c75410ae
Lugard Agu <agulugard@gmail.com>
2023-08-16 20:44:52 +0100

    Generate mvc for expense and relative migrations

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,25 +1,32 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @user = current_user
+    @categories = current_user.categories.order(created_at: :desc)
   end
 
   # GET /categories/1 or /categories/1.json
-  def show; end
+  def show
+    @user = current_user
+    @category = Category.find(params[:id])
+    @category_expense = @category.expenses
+  end
 
   # GET /categories/new
   def new
+    @user = current_user
     @category = Category.new
   end
 
   # GET /categories/1/edit
-  def edit; end
+  # def edit; end
 
   # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
+    @category.user_id = current_user.id
 
     respond_to do |format|
       if @category.save
@@ -33,34 +40,38 @@ class CategoriesController < ApplicationController
   end
 
   # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @category.update(category_params)
+  #       format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @category }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @category.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
-    @category.destroy
+    @category = Category.find(params[:id])
 
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
+      if @category.destroy
+        format.html { redirect_to categories_url, notice: 'Category was successfully deleted.' }
+        format.json { head :no_content }
+      else
+        format.html { render :new, alert: 'An error has occured while deleting category' }
+      end
     end
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_category
-    @category = Category.find(params[:id])
-  end
+  # def set_category
+  #   @category = Category.find(params[:id])
+  # end
 
   # Only allow a list of trusted parameters through.
   def category_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,17 +33,17 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @user }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /users/1 or /users/1.json
   def destroy
@@ -58,9 +58,9 @@ class UsersController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
   # Only allow a list of trusted parameters through.
   def user_params
