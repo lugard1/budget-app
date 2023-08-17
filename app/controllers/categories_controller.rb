aad5747807f174f3a17fc9806f7f3863bcc0ceb1
Lugard Agu <agulugard@gmail.com>
2023-08-17 13:16:06 +0100

    Clean code base

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,15 +20,11 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  # def edit; end
-
   # POST /categories or /categories.json
   def create
     @user = current_user
     @category = Category.new(category_params)
     @category.user_id = current_user.id
-    # @category.author = current_user
 
     if @category.save
       redirect_to categories_path, notice: 'New category has been created!'
@@ -36,30 +32,7 @@ class CategoriesController < ApplicationController
       error = @category.errors.full_messages[0]
       redirect_to new_category_path, notice: "#{error} Please try again."
     end
-
-    # respond_to do |format|
-    #   if @category.save
-    #     format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
-    #     format.json { render :show, status: :created, location: @category }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @category.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
-
-  # PATCH/PUT /categories/1 or /categories/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @category.update(category_params)
-  #       format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @category }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @category.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
@@ -76,11 +49,6 @@ class CategoriesController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_category
-  #   @category = Category.find(params[:id])
-  # end
 
   # Only allow a list of trusted parameters through.
   def category_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,19 +32,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @user.update(user_params)
-  #       format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @user }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
@@ -56,11 +43,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
 
   # Only allow a list of trusted parameters through.
   def user_params
