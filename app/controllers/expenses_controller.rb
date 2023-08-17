e6b6747c532c59aaea672f35ec0e4f741fb78a9e
Lugard Agu <agulugard@gmail.com>
2023-08-17 12:49:37 +0100

    Fix N+1 issues and rubocop errors

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -64,13 +64,14 @@ class ExpensesController < ApplicationController
   # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_expense
-    #   @expense = Expense.find(params[:id])
-    # end
 
-    # Only allow a list of trusted parameters through.
-    def expense_params
-      params.require(:expense).permit(:name, :amount, :category)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  # def set_expense
+  #   @expense = Expense.find(params[:id])
+  # end
+
+  # Only allow a list of trusted parameters through.
+  def expense_params
+    params.require(:expense).permit(:name, :amount, :category)
+  end
 end
